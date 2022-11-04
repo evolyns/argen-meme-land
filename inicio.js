865989ad5531c3ac83699017fbec8d2800b49808
Manu Vaccarelli <manuvaccarelli@outlook.com>
2022-11-04 10:32:03 -0300

    1/2 agregada la interaccion del usuario con el boton de registro, -aparece el recuadro del registro-

---
+++ inicio.js
@@ -0,0 +1,14 @@
+"use strict"
+let fecha__actualizada = "Actualizado el 03/11/22"
+alert(fecha__actualizada);
+const bloque__registro = document.querySelector(".bloque__registro");
+
+function recuadro__registro1 (){
+     bloque__registro.style.visibility="visible"
+    bloque__registro.style.animation.name="registro"
+
+}
+
+
+
+
