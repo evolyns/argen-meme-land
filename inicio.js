849e5e203737df32a31f7e669696594049b08ea4
Manu Vaccarelli <manuvaccarelli@outlook.com>
2022-11-06 19:20:35 -0300

    Agregado icono del perfil junto con una lista dentro

--- inicio.js
+++ inicio.js
@@ -1,10 +1,11 @@
 "use strict"
-let fecha__actualizada = "Actualizado el 03/11/22"
+let fecha__actualizada = "Actualizado el 06/11/22"
 const bloque__registro = document.querySelector(".bloque__registro");
 const  bloque__Isesion = document.querySelector(".bloque__Isesion");
 
 // const boton__cerrar__registro = document.querySelector(".boton__cerrar__registro");
 alert(fecha__actualizada)
+alert("Nuevos cambios: Agregado el boton del perfil del usuario en la barra de navegacion. Creada la lista dentro del icono " )
 // FUNCIONES RECUADRO REGISTRO 
 function recuadro__registro1 (){
     bloque__registro.style.visibility="visible";
