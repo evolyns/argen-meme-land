48b41ebf0b5c74e290b780693ab3c1b6f407a9d4
Manu Vaccarelli <manuvaccarelli@outlook.com>
2022-11-06 13:36:42 -0300

    recuadros registro y inicio sesion con sus respectivos botones de volver

--- inicio.js
+++ inicio.js
@@ -1,14 +1,31 @@
 "use strict"
 let fecha__actualizada = "Actualizado el 03/11/22"
-alert(fecha__actualizada);
 const bloque__registro = document.querySelector(".bloque__registro");
+const  bloque__Isesion = document.querySelector(".bloque__Isesion");
 
+// const boton__cerrar__registro = document.querySelector(".boton__cerrar__registro");
+alert(fecha__actualizada)
+// FUNCIONES RECUADRO REGISTRO 
 function recuadro__registro1 (){
-     bloque__registro.style.visibility="visible"
-    bloque__registro.style.animation.name="registro"
+    bloque__registro.style.visibility="visible";
+    return false;
 
+}
+ function cerrar__registro(){
+        bloque__registro.style.visibility="hidden"
+        return false;
+    }
+// FUNCIONES RECUADRO INICIO SESION
+
+function recuadro__Isesion (){
+    bloque__Isesion.style.visibility="visible";
+    return false;
+}
+function cerrar__Isesion (){
+    bloque__Isesion.style.visibility="hidden";
+    return false;
 }
 
 
 
-
+ fecha__actualizada = "Actualizado el 04/11/22"
